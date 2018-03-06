00e6232117baa42ed051ebbbbe0afc0aa82f25a5
Gerard <gkhynes@gmail.com>
2018-03-06 22:32:12 +0000

    Add tooltip

--- app.js
+++ app.js
@@ -51,4 +51,50 @@ d3.queue()
               drawMap(geoData, data, currentYear, currentDataType);
               drawBar(data, currentDataType, country);
           });
+
+      d3.selectAll("svg")
+          .on("mousemove touchmove", updateTooltip);
+
+      function updateTooltip() {
+          var tooltip = d3.select(".tooltip");
+          var tgt = d3.select(d3.event.target);
+          var isCountry = tgt.classed("country");
+          var isBar = tgt.classed("bar");
+          var isArc = tgt.classed("arc");
+          var dataType = d3.select("input:checked")
+                           .property("value")
+          var units = dataType === "emissions" ? "thousand metric tones" : "metric tons per capita";
+          var data;
+          var percentage = "";
+          if (isCountry) data = tgt.data()[0].properties;
+          if (isArc) {
+              data = tgt.data()[0].data;
+              percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`
+          }
+          if (isBar) data = tgt.data()[0];
+          tooltip
+            .style("opacity", +(isCountry || isArc || isBar))
+            .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+            .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+          if (data) {
+              var dataValue = data[dataType] ? data[dataType].toLocaleString() + " " + units : "Data Not Available";
+              tooltip
+                .html(`
+                    <p>Country: ${data.country}</p>
+                    <p>${formatDataType(dataType)}: ${dataValue}</p>
+                    <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+                    ${percentage}
+                `)          
+          }
+      }
   });
+
+function formatDataType(key) {
+  return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => " " + c);
+}
+
+function getPercentage(d) {
+  var angle = d.endAngle - d.startAngle;
+  var fraction = 100 * angle / (Math.PI * 2);
+  return fraction.toFixed(2) + "%";
+}
